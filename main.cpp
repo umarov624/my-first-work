7982bfbd4e964781389eeab4d811213f76e2cb0d
umarov <q.umarov@newuu.uz>
2025-01-17 12:28:48 +0500

    nigga 1 nigga 2 nigga 3 nigga 4

---
+++ main.cpp
@@ -0,0 +1,15 @@
+   #include <iostream>
+   using namespace std;
+int main() {
+   cout << "Hello, my name is Quvonchbek" << endl;
+   cin>>age;
+   cout << "My age is: " <<age << endl;
+
+
+   return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
