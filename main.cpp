bcbb2bf3746aa742e969210dac49d64c5787f165
umarov <q.umarov@newuu.uz>
2025-02-07 11:08:31 +0500

    20 ta masala 1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,236 @@
    #include <iostream>
    using namespace std;
 int main() {
-   cout << "Hello, my name is Quvonchbek" << endl;
-   cin>>age;
-   cout << "My age is: " <<age << endl;
+   // Problem 1
+   //    int num1, num2, num3, num4, num5;
+   //    cout << "enter: " << endl;
+   //    cin>>num1;
+   //    cin>>num2;
+   //    cin>>num3;
+   //    cin>>num4;
+   //    cin>>num5;
+   //    std::cout<< "sum is " << num5 + num1 + num2 + num3 + num4;
+   //    return 0;
+   // }
+
+   // Porblem 2
+   // int Student_ID;
+   // float Student_GPA;
+   // short Student_Year_of_Study;
+   // double Student_Study_fee;
+   //
+   // cout << "Enter Student ID: " << endl;
+   // cin >> Student_ID;
+   // cout << "Enter Student GPA: " << endl;
+   // cin >> Student_GPA;
+   // cout << "Enter Student Year of Study: " << endl;
+   // cin >> Student_Year_of_Study;
+   // cout << "Enter Student Study fee: " << endl;
+   // cin >> Student_Study_fee;
+   //
+   // std::cout << "Enter Student ID: " << Student_ID << endl;
+   // std::cout << "Enter Student GPA: " << Student_GPA << endl;
+   // std::cout << "Enter Student Year of Study: " << Student_Year_of_Study << endl;
+   // std::cout << "Enter Student Study fee: " << Student_Study_fee<< endl;
+
+
+
+
+   //problem 3
+   // double celcius, fahrenheit;
+   // cout << "Enter the temperature in Celcius: ";
+   // cin >> celcius;
+   // fahrenheit = (celcius * 1.8) + 32;
+   // cout << "Fahrenheit: " << fahrenheit << endl;
+
+
+   // problem 4
+   // double price;
+   // int quantity;
+   // cout << "Please enter the price: ";
+   // cin >> price;
+   // cout << endl << "Please enter the quantity: ";
+   // cin >> quantity;
+   // std::cout << "Total cost is " << price * quantity << endl;
+
+
+   // problem 5
+   // int father_age, mother_age, son_age, daughter_age;
+   // cout << "Enter your father's age: " << endl;
+   // cin >> father_age;
+   // cout << "Enter your mother's age: " << endl;
+   // cin >> mother_age;
+   // cout << "Enter your son's age: " << endl;
+   // cin >> son_age;
+   // cout << "Enter your daughter's age: " << endl;
+   // cin >> daughter_age;
+   // std::cout << "Average age is : " << father_age + son_age + mother_age + daughter_age / 4 << endl;
+
+
+   // problem 6
+   // int length, width;
+   // cout << "Please enter a length: ";
+   // cin >> length;
+   // cout << "Please enter a width: ";
+   // cin >> width;
+   // std::cout << "the area of the rectangle is : " << length * width << endl;
+   // std::cout << "the perimeter of the rectangle is : " << 2 * (length + width) << endl;
+
+
+   // problem 7
+   // int dividend, divisor, quotient, remainder;
+   // cout << "Enter dividend: " << endl;
+   // cin >> dividend;
+   // cout << "Enter divisor: " << endl;
+   // cin >> divisor;
+   // quotient = dividend / divisor;
+   // remainder = dividend % divisor;
+   // std::cout << "the quotient of the division is: " << quotient << endl;
+   // std::cout << "the remainder of the division is: " << remainder << endl;
+
+
+   // problem 8
+   // int USD, exchange_rate_of_usd, exchange_rate_of_ruble;
+   // cout<<"Enter USD: " <<endl;
+   // cin>>USD;
+   // cout<<"Enter exchange rate of usd: " <<endl;
+   // cin>>exchange_rate_of_usd;
+   //
+   // cout<<"Enter exchange rate of ruble: " <<endl;
+   // cin>>exchange_rate_of_ruble;
+   //
+   //
+   // std::cout << "the equivalent amount is UZS: " << USD * exchange_rate_of_usd << endl;
+   // std::cout << "the equivalent amount is RUBS: " << USD * exchange_rate_of_ruble << endl;
+
+   // problem 9
+   // int radius;
+   // cout << "Enter radius: " << endl;
+   // cin >> radius;
+   // std::cout << "surface area of circle = " << radius * radius * M_PI << endl;
+
+   // problem 10
+   // int radius, height;
+   // cout << "Enter radius: " << endl;
+   // cin >> radius;
+   // cout << "Enter height: " << endl;
+   // cin >> height;
+   // std::cout << "Volume of circle is: " << radius * height * radius * M_PI<< endl;
+
+   // problem 11
+        // int distance, time;
+        // std::cout<<"Enter distance: " << std::endl;
+        // std::cin>>distance;
+        // std::cout<<"Enter time: " << std::endl;
+        // std::cin>>time;
+        //
+        // std::cout<<"velocity = " << (distance / time)<< "km/h" << " and " << ((distance / time) / 1.609344) << " mile/h"<< std::endl;
+
+
+        // problem 12
+        // int total_minuts;
+        // cout << "Enter total minutes: " << endl;
+        // cin >> total_minuts;
+        // std::cout << "Total time: " << total_minuts / 60 << " hours " << total_minuts % 60 << " minutes"  << endl;
+
+
+        // problem 13
+        // int price, cost;
+        // cout<<"Enter Price : ";
+        // cin>>price;
+        // cout<<"Enter Cost : ";
+        // cin>>cost;
+        // std::cout << "break even point: " << 10000 / (price - cost) << endl;
+
+        // problem 14
+        // int basic_salary, tax_percentage;
+        // cout << "Enter your salary: " << endl;
+        // cin >> basic_salary;
+        // cout << "Enter your tax percentage: " << endl;
+        // cin >> tax_percentage;
+        // std::cout << "total salary : " << basic_salary * ( 100 - tax_percentage ) / 100 << endl;
+
+
+        // problem 15
+        // int original_price, discount_percentage;
+        // cout << "Please enter the price of your discount percentage: " << endl;
+        // cin >> original_price;
+        // cout << "Please enter the discount percentage: " << endl;
+        // cin >> discount_percentage;
+        // std::cout << "discout amount: " << discount_percentage * original_price / 100 << endl;
+        // std::cout << "final price: " << original_price - discount_percentage * original_price / 100 << endl;
+
+
+        // problem 16
+        // double total = 0;
+        // int totalCredits = 0;
+        // for(int i=0;i<5;i++) {
+        //     double grades;
+        //     cin>>grades;
+        //     int credits;
+        //     cin>>credits;
+        //     totalCredits += credits;
+        //     total+=credits*grades;
+        // }
+        // cout<<total/totalCredits;
+
+        // problem 17
+        // double final_price, initial_price;
+        // cout << "Enter the final price: " << endl;
+        // cin >> final_price;
+        // cout << "Enter the initial price: " << endl;
+        // cin >> initial_price;
+        // cout << "inflation rate: " << ((final_price - initial_price) / initial_price) * 100 << " %" << endl;
+
+
+        // problem 18
+        // int original_tuition_fee, scholarship_rate;
+        // cout << "Please enter your original tuition fee: " << endl;
+        // cin >> original_tuition_fee;
+        // cout << "Please enter your scholarship rate: " << endl;
+        // cin >> scholarship_rate;
+        // cout << "scholarship amount: " << scholarship_rate * original_tuition_fee / 100 << endl;
+        // cout << "final tuition fee: " << original_tuition_fee - (scholarship_rate * original_tuition_fee / 100) << endl;
+
+
+        // problem 19
+        // int principal_amount, rate_of_interest, time;
+        // cout << "Enter Principal amount: " << endl;
+        // cin >> principal_amount;
+        // cout << "Enter Rate of interest: " << endl;
+        // cin >> rate_of_interest;
+        // cout << "Enter Time: " << endl;
+        // cin >> time;
+        // cout << "the simple interest: " << principal_amount * time * rate_of_interest / 100 << endl;
+
+
+        // problem 20
+        // double principal, annual_rate_of_interest;
+        // cout << "Enter Principal: " << endl;
+        // cin >> principal;
+        // cout << "Enter annual rate of interest: " << endl;
+        // cin >> annual_rate_of_interest;
+        // cout << "total amount after 2 yeas: " << principal * (1 + (annual_rate_of_interest / 100)) * (1 + (annual_rate_of_interest / 100)) << endl;
+        // cout << "compound interest after 2 years: " << principal * (1 + (annual_rate_of_interest / 100)) * (1 + (annual_rate_of_interest / 100)) - principal;
+
+
+
+
+
+
+
 
 
    return 0;
 }
+
+
+
+
+
+
+
+
 
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
